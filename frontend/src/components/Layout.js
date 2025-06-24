c9f0ecc3e4646e599f120f4cebc1dd5328d3f57b
vinicks06 <viniciuscoelho0608@gmail.com>
2025-06-24 16:51:03 -0300

    Sidebar está fixa

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -5,8 +5,10 @@ import Sidebar from './Sidebar';
 export default function Layout() {
     return (
         <div className="flex min-h-screen bg-gray-100 dark:bg-slate-900">
-            <Sidebar />
-            <main className="flex-1 p-8">
+            <div className="fixed top-0 left-0 h-full">
+                <Sidebar />
+            </div>
+            <main className="flex-1 p-8 ml-64">
                 <Outlet />
             </main>
         </div>
