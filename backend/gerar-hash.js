805e33bb144a7398fce86feaaf5b76cf94a90f9e
BrunoBerwig <bruno_berwig@estudante.sesisenai.org.br>
2025-07-01 17:11:05 -0300

    ToolBars, breadcumbs

--- backend/gerar-hash.js
+++ backend/gerar-hash.js
@@ -3,14 +3,10 @@ const bcrypt = require('bcrypt');
 const senhaEmTextoPuro = 'senha'; 
 const saltRounds = 10;
 
-console.log(`Gerando hash para a senha: "${senhaEmTextoPuro}"`);
-
 bcrypt.hash(senhaEmTextoPuro, saltRounds, function(err, hash) {
     if (err) {
         console.error("Erro ao gerar hash:", err);
         return;
     }
-    console.log("\n>>> SEU HASH SEGURO: <<<\n");
     console.log(hash);
-    console.log("\nCopie a linha de hash acima e cole na coluna 'password_hash' do seu banco de dados.");
 });
